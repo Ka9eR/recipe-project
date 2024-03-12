d4738c4cce15b2a9900cdaefc167b44b9a9e61f2
Ka9eR <kcpr.parol2@gmail.com>
2024-03-12 17:10:39 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "76971063-d1ce-4b55-965e-7788bd0fcf9b";
+export const MODAL_CLOSE_SEC = 2.5;
